3a6142a4d83f4218053c7a90260de707bc91bddb
Jessie P. <ritime275@gmail.com>
2024-06-29 19:07:53 -0400

    Inserted forecast html to javaScript

--- src/index.js
+++ src/index.js
@@ -52,7 +52,33 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Sat", "Sun", "Mon", "Tues", "Wed"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="forecast-day">
+       <div class="forecast-date">${day}</div>
+       <div class="forecast-icon">🌧</div>
+       <div class="forecast-temperatures">
+         <div class="forecast-temperature">
+           <strong>26°</strong>
+         </div>
+       <div class="forecast-temperature">18°</div>
+     </div>
+    </div>
+    `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Canada");
+displayForecast();
